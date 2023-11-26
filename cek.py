04ba3acc3934638369e023b83119f16ff2ba92c1
ValentinoTriadi <valent122016@gmail.com>
2023-11-26 20:59:13 +0700

    feat(PDA): add input global attr

--- cek.py
+++ cek.py
@@ -9,8 +9,6 @@ class HTMLParser:
         self.symbol = []
         self.rule = {}
         self.parsePDA()
-        print(self.symbol)
-        print(self.state)
         # current_state:str = ''
         # state:list = []
         # rule:dict = {
@@ -36,17 +34,14 @@ class HTMLParser:
 
 
     def makeStateList(self, s):
-        global state
         temp = s.rstrip().split(" ")
         self.state = temp
 
     def makeSymbolList(self, s):
-        global symbol
         temp = s.rstrip().split(" ")
         self.symbol = temp
 
     def makeRuleDict(self, s):
-        global rule, current_state
         tempdict = {}
         fixdict = {}
         temprule = s.rstrip().split(' ')
@@ -61,8 +56,7 @@ class HTMLParser:
             self.rule[temprule[0]] = fixdict
 
     def parsePDA(self):
-        print(sys.argv[0])
-        f = open("PDA.txt")
+        f = open(sys.argv[1])
         s = f.readlines()
         self.makeStateList(s[0])
         self.makeSymbolList(s[1])
@@ -71,7 +65,8 @@ class HTMLParser:
 
     def checkSymbol(self, s):
         if not s in self.symbol and s != ' ':
-            print("\x1b[93mUnexpected Symbol\x1b[0m")
+            print(f"\x1b[91mSyntax Error!\x1b[0m")
+            print("\x1b[93mUnexpected Symbol\x1b[0m\033[94m",s,'\033[0m\033[93m!\033[0m')
             return False
         else:
             if ('@' in self.rule[self.current_state].keys()):
@@ -80,7 +75,8 @@ class HTMLParser:
                     if (s != i and i != '@'):
                         return True
             elif not s in self.rule[self.current_state].keys():
-                print(f"\x1b[93mSymbol {s} is not an input of current state\x1b[0m")
+                print(f"\x1b[91mSyntax Error!\x1b[0m")
+                print(f"\x1b[93mSymbol \033[94m{s}\033[93m is not an input of current state\x1b[0m")
                 return False
 
         return True
@@ -123,7 +119,6 @@ class HTMLParser:
         s = f.read()
         f.close()
         # s = s.replace('\n', '')
-        print(s)
         line = 1
         char = 0
         temp = ""
@@ -146,22 +141,42 @@ class HTMLParser:
                     return line, char
                 if (not self.modifyStack(cc)):
                     if (cc == '+'):
-                        cc == "Space"
-                    print("Gabener woi tag nya",'\033[95m',cc, '\x1b[0m', ", harusnya",self.stack[-1])
+                        cc = "Space"
+                    print("\033[91mSyntax Error!\033[0m")
+                    print("Tag Salah!",'Symbol\033[95m',cc + '\x1b[0m'+ " seharusnya adalah symbol\033[95m", self.stack[-1], '\033[0m')
                     return line, char 
                 newstack = self.reversestack(self.stack)
                 print('\x1b[41m',cc, newstack, self.current_state,'\x1b[0m')
-        print(temp)
         return -1, -1
 
 html_parser = HTMLParser()
-status = html_parser.parseHTML("html.html")
+status = html_parser.parseHTML(sys.argv[2])
 
 if status[0] != -1:
-    print('\033[96m'+"FAIL"+'\033[0m'+" on line", '\033[91m', status[0],'\033[0m', "char ke", '\033[91m', status[1],'\033[0m', "On state",'\033[94m', html_parser.current_state, '\033[0m')
+    print('\033[96m'+"FAIL"+'\033[0m'+" on line", '\033[93m', status[0],'\033[0m', "char ke", '\033[93m', status[1],'\033[0m', "On state",'\033[94m', html_parser.current_state, '\033[0m')
+    f = open(sys.argv[2])
+    lines = ""
+    for i in range(status[0]):
+        lines = f.readline()
+    count = 0
+    ignoreBlank = True
+    print('\033[97mError: \033[0m', end='')
+    for c in lines:
+        count+=1
+        if (c == ' ' and ignoreBlank):
+            continue
+        else:
+            ignoreBlank = False
+            if (count != status[1]):
+                print(c,end='')
+            else:
+                if (c != ' '):
+                    print('\033[91m'+c+'\033[0m', end='')
+                else:
+                    print('\033[91m'+'_'+'\033[0m', end='')
 else:
     if (html_parser.stack[-1] == 'Z'):
-        print('\033[92m'+"SUCCESS"+'\033[0m')
+        print('\033[92m'+"Accepted"+'\033[0m')
     else:
         print('\033[93m'+"Stack is not empty!"+'\033[0m')
 
